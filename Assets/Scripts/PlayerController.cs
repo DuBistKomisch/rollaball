39a62fc1dbfff4af1992b23f4b692b07ef59c750
Jake Barnes <jakerodneybarnes@gmail.com>
2017-08-25 12:15:59 +1000

    After Step 7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
   public float speed;
 
+  public Text countText;
+  public Text winText;
+
   private Rigidbody rb;
+
+  private int count;
+  private int Count {
+    get {
+      return this.count;
+    }
+    set {
+      this.count = value;
+      this.UpdateCount();
+    }
+  }
 
   void Start() {
     this.rb = this.GetComponent<Rigidbody>();
+    this.Count = 0;
   }
 
   void FixedUpdate() {
@@ -23,6 +39,12 @@ public class PlayerController : MonoBehaviour {
   void OnTriggerEnter(Collider other) {
     if (other.gameObject.CompareTag("Pick Up")) {
       other.gameObject.SetActive(false);
+      this.Count++;
     }
+  }
+
+  private void UpdateCount() {
+    this.countText.text = "Count: " + this.Count.ToString();
+    this.winText.text = this.Count >= 6 ? "You Win!" : "";
   }
 }
