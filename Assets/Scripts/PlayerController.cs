5320df82cd7557d41a157646e6a98e300ca2c231
Jake Barnes <jakerodneybarnes@gmail.com>
2017-08-25 11:55:57 +1000

    After Step 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,4 +19,10 @@ public class PlayerController : MonoBehaviour {
 
     this.rb.AddForce(movement * this.speed);
   }
+
+  void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("Pick Up")) {
+      other.gameObject.SetActive(false);
+    }
+  }
 }
